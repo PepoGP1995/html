9011aa62bb99b647d9776f3917e70dac82a1f630
PepoGP1995 <pepofilms@gmail.com>
2021-11-30 15:56:29 -0500

    asdasg

---
+++ app2php.js
@@ -0,0 +1,9 @@
+const num1=document.querySelector('.Numero1');
+const num2=document.querySelector('.Numero2');
+const boton=document.querySelector('.boton')
+const disp=document.querySelector('.display');
+
+boton.onclick = () => {
+    let suma=parseFloat(num1.value) +parseFloat(num2.value); 
+    disp.innerHTML=suma;
+}
--- usuario.php
+++ usuario.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,39 +8,38 @@
     <link rel="stylesheet" href="estilos/stylesphp.css">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-    $variable1=2;    
-    $variable2=3; 
-    $variable3='hola'; 
+    <?php
+    $variable1 = 2;
+    $variable2 = 3;
+    $variable3 = 'hola';
 
-?>
-<table style="border: 1px solid black;" class="EstiloTabla">
-<tr>
-    <th>variable1</th>
-    <th>variable2</th>
-    <th>variable3</th>
-</tr>
-<tr>
-    <td>
-        <?php
-            echo $variable1;
-        ?>
-    </td>
-    <td>
-        <?php
-            echo $variable2;
-        ?>
-    </td>
-    <td>
+    ?>
+    <div class="Container">
+        <h1>Laboratorio 1</h1>
+        <div class="Datos">
+            <p class="Numero1">
+                <?php
+                echo $variable1;
+                ?>
+            </p>
+            <p class="Numero2">
+                <?php
+                echo $variable2;
+                ?>
+            </p>
+            <button type="submit" class="boton">Enviar</button>
 
-        <?php
-         echo $variable3;
-        ?>
-    
-    </td>
-</tr>
-</table>
+        </div>
+        <div class="Resultado">
+            <br>
+            <h1>Resultado</h1>
+            <p class="display">0.0</p>
+        </div>
+    </div>
+    <script src="app2php.js"></script>
 
 </body>
+
 </html>
