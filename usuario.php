5b3750a6c950cb4d714fccd8d71eff4f5d1109e1
PepoGP1995 <pepofilms@gmail.com>
2021-11-30 15:35:07 -0500

    a

--- usuario.php
+++ usuario.php
@@ -1,4 +1,43 @@
-<?php>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/stylesphp.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $variable1=2;    
+    $variable2=3; 
+    $variable2=7; 
 
 ?>
+<table style="border: 1px solid black;">
+<tr>
+    <th>variable1</th>
+    <th>variable2</th>
+    <th>variable3</th>
+</tr>
+<tr>
+    <td>
+    <?php
+    echo $variable1;
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $variable2;
+    ?>
+    </td>
+    <td>
+    <?php
+    echo $variable3;
+    ?>
+    </td>
+</tr>
+</table>
+
+</body>
+</html>
