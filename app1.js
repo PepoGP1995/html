43581d5b27f3341d5f5c12a90da2c85800be7519
PepoGP1995 <pepofilms@gmail.com>
2021-11-30 15:09:43 -0500

    prueba

---
+++ app1.js
@@ -0,0 +1,10 @@
+const num1=document.querySelector('.Numero1');
+const num2=document.querySelector('.Numero2');
+const boton=document.querySelector('.boton')
+const disp=document.querySelector('.display');
+
+boton.onclick = () => {
+    console.log(num1.value);
+    let suma=parseFloat(num1.value) +parseFloat(num2.value); 
+    disp.innerHTML=suma;
+}
