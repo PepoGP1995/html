8a96e2e646272c3965416918ee3d43924ec93262
PepoGP1995 <pepofilms@gmail.com>
2021-12-03 15:13:08 -0500

    dfad

---
+++ practica3.php
@@ -0,0 +1,11 @@
+<?php
+
+$variable1=1;
+$variable2=2;
+if ($variable1>$variable2){
+    echo "Variable 1 es Mayor que la Variable 2";
+}else{
+    echo "Variable 2 es Mayor que la Variable 1";
+}
+
+?>                           
