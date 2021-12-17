d84fc9a9928b53c08c23d4e85343f5c81459e95d
PepoGP1995 <pepofilms@gmail.com>
2021-12-17 18:31:11 -0500

    sdfsd

--- formulariophp7/Highcharts-9.3.2/examples/line-basic/control.php
+++ formulariophp7/Highcharts-9.3.2/examples/line-basic/control.php
@@ -53,9 +53,9 @@ while($fila= mysqli_fetch_array($result)){?>
 </head>
 <body>
     <br>
-<button onclick="location.href='lineal.php'" value="New Tab" target="_blank">Grafica Lineal</button>
+<button onclick="location.href='lineal.php'" value="New Tab" target="_blank"><span class="icon">Grafica Lineal</span></button>
 <br>
-<button onclick="location.href='gauge.php'" value="New Tab" target="_blank">Grafica Gauge</button>
+<button onclick="location.href='gauge.php'" value="New Tab" target="_blank"><span class="icon">Grafica Gauge</span></button>
 </body>
 </html>
 
