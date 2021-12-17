46f36effa91791641cf402ac5f7bf39e8f5f8c0c
PepoGP1995 <pepofilms@gmail.com>
2021-12-17 18:17:36 -0500

    dfgdfgfd

--- formulariophp7/Highcharts-9.3.2/examples/line-basic/control.php
+++ formulariophp7/Highcharts-9.3.2/examples/line-basic/control.php
@@ -1,5 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
+<body>
 include_once( $_SERVER['DOCUMENT_ROOT'] . "/formulariophp7/Highcharts-9.3.2/examples/line-basic/config.inc.php" );
 include_once(DIR_INC . "class.mysql.inc.php");
 $bd = new class_mysql();
@@ -43,3 +51,12 @@ while($fila= mysqli_fetch_array($result)){?>
 //$bd->insertar(2,2,2);
 
 ?>
+
+<button onclick="location.href='lineal.php'">Grafica Lineal</button>
+<br>
+<button onclick="location.href='gauge.php'">Grafica Gauge</button>
+</body>
+</html>
+<?php
+
+
