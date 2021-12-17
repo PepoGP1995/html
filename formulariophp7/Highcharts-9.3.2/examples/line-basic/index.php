c2a1bd3a6cd32b3232fa04c50cec5a78c54498d6
PepoGP1995 <pepofilms@gmail.com>
2021-12-17 16:23:05 -0500

    fghfghfg

--- formulariophp7/Highcharts-9.3.2/examples/line-basic/index.php
+++ formulariophp7/Highcharts-9.3.2/examples/line-basic/index.php
@@ -71,7 +71,7 @@
 
 <?php
 
-include_once( $_SERVER['DOCUMENT_ROOT'] . "/line-basic/config.inc.php" );
+include_once( $_SERVER['DOCUMENT_ROOT'] . "/formulariophp7/Highcharts-9.3.2/examples/line-basic/config.inc.php" );
 include_once(DIR_INC . "class.mysql.inc.php");
 $bd = new class_mysql();
 
@@ -139,6 +139,5 @@ Highcharts.chart('container', {
 
 });
 		</script>
-<?php echo $result; ?>
 	</body>
 </html>
