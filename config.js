d45b6756a749a62a81271b5a14c2cea71204d31d
PepoGP1995 <pepofilms@gmail.com>
2021-12-21 15:43:29 -0500

    fgdhdfg

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '192.168.100.42';	// hostname or IP address
+host = '192.168.0.109';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
 port = 9001;
-topic = 'casa/cocina/nevera';		// topic to subscribe to
+topic = 'casa/cocina/refrigeradora';		// topic to subscribe to
 useTLS = false;
 username = null;
 password = null;
