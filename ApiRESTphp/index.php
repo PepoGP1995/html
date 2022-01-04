8a61e4c4bde15c7fc18ec3e7705264f0de3799d9
PepoGP1995 <pepofilms@gmail.com>
2022-01-04 15:38:50 -0500

    sdfsdf

--- ApiRESTphp/index.php
+++ ApiRESTphp/index.php
@@ -1,16 +1,11 @@
 <?php
-$url  = file_get_contents('https://api.giphy.com/v1/gifs/search?api_key=Sg5olltEJfU9II6z674VKqy5fOEfXV4O&q=emelec&limit=10');
-$json = json_decode( $url );
-foreach ( $json as $values ) {
- 
-    foreach ( $values as $value ) {
 
-        if ( isset( $value->images->original->url ) ) {
-
+$url = file_get_contents('https://api.giphy.com/v1/gifs/search?api_key=Sg5olltEJfU9II6z674VKqy5fOEfXV4O&q=pornhub&limit=10');
+$json = json_decode($url);
+foreach ($json as $values) {
+    foreach ($values as $value) {
+        if (isset($value->images->original->url)) {
             echo "<img src='{$value->images->original->url}' width='100' height='100'>";
         }
     }
 }
-
-
-?>
